8cc202cea792b67d3811b35253cb1decb293d4a0
hamidou <hamidouboum@hotmail.fr>
2025-10-06 13:40:44 +0100

    fixing projects

--- backend/src/middlewares/verifyToken.js
+++ backend/src/middlewares/verifyToken.js
@@ -3,6 +3,9 @@ const jwt = require("jsonwebtoken");
 const verifyToken = (req, res, next) => {
   try {
     const authorizationHeader = req.get("Authorization");
+    if (authorizationHeader == null) {
+      throw new Error("Authorization header is missing");
+    }
 
     const [type, token] = authorizationHeader.split(" ");
 
